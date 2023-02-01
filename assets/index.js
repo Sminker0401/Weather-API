e7518ef6c4f3d39f9190fa6fd872beb42f28e115
Stephanie Minker <sminker0401@gmail.com>
2023-02-01 14:08:13 -0500

    fixed bugs

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,32 @@
 let weather = {
     apiKey:"ee8dcb8d655a4bb76ff89dc3e55fc9e8",
     FetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=[CITY]&units=imperial&appid=={ee8dcb8d655a4bb76ff89dc3e55fc9e8}")
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid="+ this.apiKey)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data))
     },
     displayWeather: function(data) {
         const { name } = data;
-        const { icon, description } = data.weather;
+        const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         document.querySelector(".city").innerText = "Weather in" + name;
-        document.querySelector(".icon").src = 
-        "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
-        document.querySelector(".winds").innerText = "Wind Speed:" + speed + "mph";
+        // document.querySelector(".winds").innerText = "Wind Speed:" + speed + "mph";
+        document.querySelector(".weather").classList.remove("loading");
+        },
+        search: function () {
+            this.FetchWeather(document.querySelector(".search-bar").value);
 
-    },
-}
+        },
+        
+
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+
