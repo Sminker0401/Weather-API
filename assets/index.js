8062ada52d3312439b61b01f44e422fa7a06da97
Stephanie Minker <sminker0401@gmail.com>
2023-02-02 23:54:48 -0500

    reworked API calls

--- assets/index.js
+++ assets/index.js
@@ -1,35 +1,45 @@
-let weather = {
-    apiKey:"ee8dcb8d655a4bb76ff89dc3e55fc9e8",
-    FetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid="+ this.apiKey)
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data))
-    },
-    displayWeather: function(data) {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°F";
-        document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
-        document.querySelector(".winds").innerText = "Wind Speed:" + speed + "mph";
-        document.querySelector(".weather").classList.remove("loading");
-        },
-        search: function () {
-            this.FetchWeather(document.querySelector(".search-btn").value);
+ const apiKey="ee8dcb8d655a4bb76ff89dc3e55fc9e8"
+ 
+ let cityName = document.querySelector("#city-name");
+ let currDay = document.querySelector("#thedate");
+ let weatherImg = document.querySelector("#weather-img");  
+ let currTemp = document.querySelector("#temp");
+ let currWind = document.querySelector("#wind");
+ let currHumidity = document.querySelector("#humidity");
+ let showRightSide = document.querySelector(".outer-div-right");
+ let cityArray = JSON.parse(localStorage.getItem("pastCitySearches")) || [];
+ let searchcity = document.getElementById("search-input"); 
 
-        },
-        
+ let searchBtn = document.getElementById("search-btn"); 
+ searchBtn.addEventListener("click", function(){
+   if (document.getElementById("search-input").value != "") {
+     handleSearch(searchcity.value);
+   } else {
+     alert("Please enter a city value in the textbox.");
+    }  
+ });
 
-};
+ function searchfor(city) {
 
-document.querySelector(".search-btn").addEventListener("click", function () {
-    weather.search();
-});
+    fetch (`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
 
-document.querySelector(".search-btn").addEventListener("click", function () {
-    weather.FetchWeather("city");
-});
+.then(function(response){
+    let data= response.json();
+    return data;
+})
+.then(function(returnData){
+    let lat= returnData[0].lat;
+    let lon= returnData[0].lon;
+})
+
+
+ }
+    
+// document.querySelector(".search-btn").addEventListener("click", function () {
+//     weather.search();
+// });
+
+// document.querySelector(".search-btn").addEventListener("click", function () {
+//     weather.FetchWeather("city");
+// });
 
