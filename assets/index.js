18c5058e844f99678cc16c7dc84c1b85ba80a289
Stephanie Minker <sminker0401@gmail.com>
2023-02-03 00:54:43 -0500

    completed javascript

--- assets/index.js
+++ assets/index.js
@@ -30,7 +30,62 @@
 .then(function(returnData){
     let lat= returnData[0].lat;
     let lon= returnData[0].lon;
-})
+
+    function chunk (arr, len) {
+        var chunks = [],
+            i = 0,
+            n = arr.length;
+        while (i < n) {
+          chunks.push(arr.slice(i, i += len));
+        }
+        return chunks;
+      }
+      const formatDate = (str) => str.slice(5,7) + '/' + str.slice(8,10) + '/' + str.slice(0,4);
+        fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+        ) 
+          .then((response) => {  //this is another way to do what we did above 
+            return response.json();
+          })
+          .then((returnData) => {
+            let desc = returnData.list[0].weather[0].description;
+            let icon = returnData.list[0].weather[0].icon;
+            let theday = formatDate(returnData.list[0].dt_txt);
+           
+            let city = returnData.city.name;
+            let temp = returnData.list[0].main.temp;
+            let wind = returnData.list[0].wind.speed;
+            let humidity = returnData.list[0].main.humidity;
+            weatherImg.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+            //use backticks to make template literal so spcace will not be ignored
+            //use innerHTML instead of textContent so interpreted correctly not as text
+            currDay.innerHTML = `&nbsp;(` + theday + `)`;
+            cityName.textContent = city;
+            currTemp.textContent = temp + ' °F';
+            currWind.textContent = wind + ' MPH';
+            currHumidity.textContent = humidity + ' %';
+            const todaysDate = returnData.list[0].dt_txt
+            const daysAfterToday = returnData.list.filter(item => item.dt_txt !== todaysDate)
+            let daysArray = chunk(daysAfterToday, 8)
+            daysArray.forEach((day, idx) => {
+              const today = day[2]
+              document.querySelector(`#weather-img${idx}`).src = `https://openweathermap.org/img/wn/${today.weather[0].icon}@2x.png`;
+              document.querySelector(`#thedate${idx}`).innerHTML = formatDate(today.dt_txt);
+              document.querySelector(`#temp${idx}`).innerHTML = today.main.temp + ' °F';
+              document.querySelector(`#wind${idx}`).innerHTML = today.wind.speed + ' MPH';
+              document.querySelector(`#humidity${idx}`).innerHTML = today.main.humidity + ' %';
+            })
+            saveCityData(city);           
+            showRightSide.classList.remove("hide");   
+            createCityButtons();
+          })
+          .catch(err => {
+            console.log(err);  
+          });
+      })
+      .catch(function(err) {
+        return err;
+      });
 
 
 } //end of function searchfor
@@ -54,7 +109,7 @@ function createCityButtons(){
 
 function saveCityData(city){
     if(!cityArray.includes(city)){
-      cityArray.unshift(city);  
+      cityArray.unshift(city);  //this saves in the beginning of the array rather then the end
       localStorage.setItem("pastCitySearches", JSON.stringify(cityArray.slice(0,8))); 
     }
   }
